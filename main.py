b4d4b1128fec3c30345f39f9048f31abbd856b3b
Misael <qmisael1023@gmail.com>
2021-10-11 20:34:02 -0700

    Modifique funcion get words Paso 4

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import palabras 
+import random
 
-def get_word(index):
+
+def get_word():
    #words
-   return palabras.words[index]
+   return random.choice(palabras.words)
    
+print(get_word())
+# palabra=get_word(3)
+# print (get_word(3))
+# print(palabra)
 
-palabra=get_word(3)
-print (get_word(3))
-print(palabra)
+#Obtener datos random de una lista 
+
+
