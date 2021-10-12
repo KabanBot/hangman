af5b5e8f44c1c303f931d096d06669b9231aabe2
AlanVG98 <78781219+AlanVG98@users.noreply.github.com>
2021-10-11 20:40:42 -0700

    Update palabras.py
    Actualizacion de palabras.py

--- palabras.py
+++ palabras.py
@@ -1,1 +1,1 @@
-words = ["Programacion","Escritura","Conducir","Tareas","Series"]
+words = ["Programacion","Escritura","Conducir","Tareas","Series","Autos","Tornado","UABC","Veloci_raptor","Habitacion312","Monterrey","Tren_Loco","Perros.calientes.de.la.cacho","La-Uabc/rifa"]
